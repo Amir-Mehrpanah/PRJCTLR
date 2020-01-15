41eab320afbe77dadd82563cc069bc2b2c1b202b
Amir Mehrpanah <hbit12233@gmail.com>
2020-01-15 10:43:59 +0330

    10th

---
+++ prj10.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+std::vector < int >
+get_primes (int);
+
+int
+main ()
+{
+  std::vector < int >primes = get_primes (2000000);
+  long cmsum = 0;
+for (int i:primes)
+    cmsum += i;
+  cout << cmsum;
+  return 0;
+}
+
+std::vector < int >
+get_primes (int max)
+{
+  std::vector < int >primes = { 2, 3, 5, 7, 11, 13, 17 };
+  int item = 19;
+A:while (item <= max)
+    {
+      int sqrtitem = sqrt (item) + 1;
+    for (int i:primes)
+	{
+	  if (i > sqrtitem)
+	    break;
+	  if (item % i == 0)
+	    {
+	      item += 2;
+	      goto A;
+	    }
+	}
+      primes.push_back (item);
+      item += 2;
+    }
+  return primes;
+}
