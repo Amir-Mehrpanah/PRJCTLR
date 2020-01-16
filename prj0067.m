089c9337e480053ff1285615259fd7a1dbd69854
Amir Mehrpanah <hbit12233@gmail.com>
2020-01-16 20:04:06 +0330

    prblm 18 & 67 slvd.

---
+++ prj0067.m
@@ -0,0 +1,23 @@
+﻿big = 100
+t = zeros(big);
+c = 1;
+for i = 2:big + 1
+    temp  = textread('content/d0067.txt','%d',c);
+    for j = 1:i-1
+        t(i-1,j) = temp(j + c - i + 1);
+    end
+    c += i;
+end
+
+for i = 2:big
+    t(i,1) += t(i-1,1);
+    t(i,i) += t(i-1,i-1);
+end
+
+for i = 3:big
+    for j = 2:i-1
+        t(i,j) += max(t(i-1,j-1),t(i-1,j));
+    end
+end
+
+max(t(end,:))
