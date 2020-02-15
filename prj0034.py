0eeaaa76e679f43bf3cac771ced140d48e4e0ecc
Amir Mehrpanah <hbit12233@gmail.com>
2020-02-15 20:57:53 +0330

    prblm34slvd

---
+++ prj0034.py
@@ -0,0 +1,11 @@
+from tqdm import tqdm
+import math
+if __name__ == "__main__":
+    sum_ = 0
+    for i in tqdm(range(3, 1000000)):
+        s = 0
+        for d in str(i):
+            s += math.factorial(int(d))
+        if s == i:
+            sum_ += i
+    print(sum_)
